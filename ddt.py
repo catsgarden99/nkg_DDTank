e914602c79c239080586bc5bfaf1779b463a7e4e
catsgarden <1476125515@qq.com>
2022-07-27 07:54:28 +0800

    1

--- ddt.py
+++ ddt.py
@@ -11,7 +11,7 @@ def checkContains():
     #     print(key,':',value,',',end='',flush=True)
     # print('',end='\r',flush=True)
 
-def doAction():
+def doActionSingle4():
     global statue
     global paiCount
     global currentTime
@@ -46,17 +46,66 @@ def doAction():
         print('打人')
         currentTime = time.time()
 
+def doActionBoGu():
+    global statue
+    global paiCount
+    global currentTime
+    # 选关
+    if statueDict['bogu.png'] != None:
+        pyautogui.click(statueDict['bogu.png'].x,statueDict['bogu.png'].y,clicks=1,interval=0.2,duration=0.2,button="left")
+        statue = 0
+        paiCount = 0
+        print('开始')
+    # 开始，展开，确认需单击
+    if statueDict['start.png'] != None:
+        pyautogui.click(statueDict['start.png'].x,statueDict['start.png'].y,clicks=1,interval=0.2,duration=0.2,button="left")
+        statue = 0
+        paiCount = 0
+        print('开始')
+    if statueDict['unexpand.png'] != None:
+        pyautogui.click(statueDict['unexpand.png'].x,statueDict['unexpand.png'].y,clicks=1,interval=0.2,duration=0.2,button="left")
+        print('收缩道具栏')
+    if statueDict['pai.png'] != None and paiCount < 3:
+        pyautogui.click(statueDict['pai.png'].x,statueDict['pai.png'].y,clicks=1,interval=0.2,duration=0.2,button="left")
+        paiCount += 1
+        print('翻牌:',paiCount)
+    if statueDict['confirm.png'] != None:
+        pyautogui.click(statueDict['confirm.png'].x,statueDict['confirm.png'].y,clicks=1,interval=0.2,duration=0.2,button="left")
+        print('花钱')
+    # 角度+5，向右
+    if statueDict['person.png'] != None and statue < 1:
+        # pyautogui.press(['up','up','up','up','up'],interval=0.1)
+        print(statueDict['person.png'].x)
+    # 道具+发力
+    if statueDict['person.png'] != None and statue < 2 and time.time()-currentTime>10:
+        pyautogui.typewrite('124',interval=0.2)
+        time.sleep(0.2)
+        pyautogui.keyDown('space')
+        time.sleep(1.7)
+        pyautogui.keyUp('space')
+        time.sleep(0.1)
+        pyautogui.press(['up','up','up','up','up'],interval=0.1)
+        time.sleep(0.5)
+        pyautogui.press(['up','up','up','up','up'],interval=0.1)
+        time.sleep(0.5)
+        pyautogui.press(['up','up','up','up','up'],interval=0.1)
+        statue += 1
+        print('打人')
+        currentTime = time.time()
+
 
 if __name__ == '__main__':
     # 创建图片存在状态字典,key为图片名,value为location
-    statueDict = {'start.png':'','fly.png':'','unexpand.png':'','pai.png':'','person.png':'','pay.png':'','confirm.png':''}
+    # statueDict = {'start.png':'','fly.png':'','unexpand.png':'','pai.png':'','person.png':'','pay.png':'','confirm.png':''}
+    statueDict = {'start.png':'','people.png':'','unexpand.png':'','pai.png':'','person.png':'','confirm.png':'','bogu.png':'','run.png':''}
     statue = 0
     paiCount = 0
     currentTime = time.time()
 
     while True:
         checkContains()
-        doAction()
+        # doActionSingle4()
+        doActionBoGu()
 
     # key=input('选择功能: 1.做一次 2.循环到死 \n')
     # if key=='1':
